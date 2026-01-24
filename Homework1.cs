72d3696cdec695782106041e9fe9fe3b68a8b1db
pj1163928 <pwjanota1@buffs.wtamu.edu>
2026-01-24 11:05:40 -0600

    Homework1 Submission

---
+++ Homework1.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Given values
+        float x = 2.5f;
+        float y = 3.3f;
+
+        // Calulate Z
+        float z = 4 * x * x + 3 * y;
+
+        //Writes the result to the console
+        System.Console.WriteLine($"The value of Z is: {z}");
+    }
+}
